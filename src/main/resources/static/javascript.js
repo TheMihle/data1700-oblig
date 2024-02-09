a503c3fcc49388a61563dc999c738c33c83ba31f
TheMihle <32242447+TheMihle@users.noreply.github.com>
2024-02-09 13:45:10 +0100

    Initial commit

---
+++ src/main/resources/static/javascript.js
@@ -0,0 +1,49 @@
+
+
+let orders = [];
+function summitOrder() {
+
+    // Reads input fields in to an object
+    let order = {
+       movie : document.getElementById("movie").value,
+       number : document.getElementById("number").value,
+       firstName : document.getElementById("first-name").value,
+       lastName : document.getElementById("last-name").value,
+       phoneNumber : document.getElementById("phone-number").value,
+       email : document.getElementById("email").value
+    }
+
+    console.log(order);
+
+
+    orders.push(order);
+
+    document.getElementById("ticket-list").innerHTML = createOrderTable(orders);
+
+
+    // Clear input fields
+    document.getElementById("number").value = "";
+    document.getElementById("first-name").value = "";
+    document.getElementById("last-name").value = "";
+    document.getElementById("phone-number").value = "";
+    document.getElementById("email").value = "";
+}
+
+function deleteOrders() {
+    orders = [];
+    document.getElementById("ticket-list").innerHTML = "";
+}
+
+// Returns a html table with all contents of order array
+function createOrderTable(orders) {
+    let table = "<table> <tr><th>Movie:</th><th>Number:</th><th>First Name:</th><th>Last Name:</th><th>Phone number:</th><th>Email:</th></tr>";
+
+    for (let order of orders) {
+        table += "<tr><td>" + order.movie + "</td><td>" + order.number + "</td><td>" + order.firstName + "</td><td>" + order.lastName + "</td><td>" + order.phoneNumber + "</td><td>" + order.email + "</td></tr>";
+    }
+
+    table += "<table>"
+
+    return table;
+
+}
