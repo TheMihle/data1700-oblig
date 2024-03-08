063e9189cf1d2ffce88dc6c01784b4ab3d1f58a7
TheMihle <32242447+TheMihle@users.noreply.github.com>
2024-03-08 17:22:47 +0100

    Added REST Controller

---
+++ src/main/java/org/example/data1700oblig1/CinemaController.java
@@ -0,0 +1,18 @@
+package org.example.data1700oblig1;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CinemaController {
+
+    @PostMapping("/delete")
+    public void deleteOrders() {
+
+    }
+
+    @PostMapping("/sumit")
+    public void summit() {
+
+    }
+}
