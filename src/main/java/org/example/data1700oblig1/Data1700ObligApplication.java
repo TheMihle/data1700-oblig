3d1a464bf877f8abc010b622219d12883734ba75
TheMihle <32242447+TheMihle@users.noreply.github.com>
2024-03-12 17:16:44 +0100

    Change name of project

--- src/main/java/org/example/data1700oblig1/Data1700ObligApplication.java
+++ src/main/java/org/example/data1700oblig1/Data1700ObligApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Data1700Oblig1Application {
+public class Data1700ObligApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Data1700Oblig1Application.class, args);
+        SpringApplication.run(Data1700ObligApplication.class, args);
     }
 
 }
