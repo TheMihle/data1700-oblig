8300ecb216715062491288156294432ac9a69b3b
TheMihle <32242447+TheMihle@users.noreply.github.com>
2024-04-10 17:50:21 +0200

    Moved storing of movies to database

---
+++ src/main/java/org/example/data1700oblig/Movie.java
@@ -0,0 +1,21 @@
+package org.example.data1700oblig;
+
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public Movie() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
