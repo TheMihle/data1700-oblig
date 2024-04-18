f9cfca8f2986e0b2ca38e61432bb20e46df51173
TheMihle <32242447+TheMihle@users.noreply.github.com>
2024-04-18 16:52:00 +0200

    Added editing of individual orders

--- src/main/resources/static/javascript.js
+++ src/main/resources/static/javascript.js
@@ -11,6 +11,7 @@ function populateMovies(movies){
         html += "<option>" + movie.title + "</option>";
     }
     document.getElementById("movie").innerHTML += html;
+    document.getElementById("movie-update").innerHTML += html;
 }
 
 // Updates the table from server
@@ -41,6 +42,57 @@ function summitOrder() {
 
     // Clear input fields
     clearInputs()
+}
+
+// Enable editing of an order
+function updateSelectOrder(id) {
+    let order;
+    $.get("/getOrder?id=" + id)
+        .then(data => {
+        order = {
+            id : data.id,
+            movie : data.movie,
+            amount : data.amount,
+            firstName : data.firstName,
+            lastName : data.lastName,
+            phoneNumber : data.phoneNumber,
+            email : data.email
+        }
+    }).done(() => {
+        document.getElementById("id-update").value = order.id;
+        document.getElementById("movie-update").value = order.movie;
+        document.getElementById("amount-update").value = order.amount;
+        document.getElementById("first-name-update").value = order.firstName;
+        document.getElementById("last-name-update").value = order.lastName;
+        document.getElementById("phone-number-update").value = order.phoneNumber;
+        document.getElementById("email-update").value = order.email;
+    });
+
+    // Display edit order form
+    document.getElementById("update-form").style.display = "block";
+
+}
+
+// Updates the order
+function updateOrder() {
+     // Reads input fields in to an object
+    const order = {
+        id : document.getElementById("id-update").value,
+        movie : document.getElementById("movie-update").value,
+        amount : document.getElementById("amount-update").value,
+        firstName : document.getElementById("first-name-update").value,
+        lastName : document.getElementById("last-name-update").value,
+        phoneNumber : document.getElementById("phone-number-update").value,
+        email : document.getElementById("email-update").value
+    }
+
+    $.post("/updateOrder", order).done(() =>{
+        updateTable()
+    }).catch(() => errorMessagee("Could not reach server, try again later"))
+
+    // Hides edit order form
+    document.getElementById("update-form").style.display = "none"
+
 }
 
 // Clear input fields
@@ -80,6 +132,7 @@ function createOrderTable(orders) {
                         "<th>Last Name:</th>" +
                         "<th>Phone number:</th>" +
                         "<th>Email:</th>" +
+                        "<th>Edit:</th>" +
                         "<th>Delete:</th></tr>";
     for (const order of orders) {
         table += "<tr>" +
@@ -89,6 +142,7 @@ function createOrderTable(orders) {
                 `<td>${order.lastName}</td>` +
                 `<td>${order.phoneNumber}</td>` +
                 `<td>${order.email}</td>` +
+                `<td><button class='btn btn-success' onclick='updateSelectOrder(${order.id})'>Edit</button>` +
                 `<td><button class='btn btn-danger' onclick='deleteOrder(${order.id})'>Delete</button></td>` +
                 "</tr>";
     }
