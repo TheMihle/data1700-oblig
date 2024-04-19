3c504dda44628a3ee3f887ed7c833a835a274100
TheMihle <32242447+TheMihle@users.noreply.github.com>
2024-04-19 12:49:32 +0200

    Improved sql schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,19 @@
-CREATE TABLE IF NOT EXISTS Orders (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    movie VARCHAR(64) NOT NULL,
-    amount SMALLINT NOT NULL,
-    firstName VARCHAR(64) NOT NULL,
-    lastName VARCHAR(64) NOT NULL,
-    phoneNumber VARCHAR(12) NOT NULL,
-    email VARCHAR(64) NOT NULL
+CREATE TABLE IF NOT EXISTS `Orders` (
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `movie` VARCHAR(64) NOT NULL,
+    `amount` SMALLINT NOT NULL,
+    `firstName` VARCHAR(64) NOT NULL,
+    `lastName` VARCHAR(64) NOT NULL,
+    `phoneNumber` VARCHAR(12) NOT NULL,
+    `email` VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Movies (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(128) NOT NULL
+CREATE TABLE IF NOT EXISTS `Movies` (
+    `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `title` VARCHAR(128) NOT NULL
 );
 
-INSERT INTO Movies (title) VALUES ('Interstellar'),
+INSERT INTO Movies (`title`) VALUES ('Interstellar'),
         ('Arrival'),
         ('Captain Philips'),
         ('Everything Everywhere All At Once'),
